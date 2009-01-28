6466ce552b8cf9e5c2548645016846b7697588eb
Stephen Hemminger <stephen.hemminger@vyatta.com>
2009-01-28 08:56:17 -0800

    check-as-prepend: turn on strict
    And reindent.

--- scripts/policy/vyatta-check-as-prepend.pl
+++ scripts/policy/vyatta-check-as-prepend.pl
@@ -1,10 +1,11 @@
 #!/usr/bin/perl
-@as_list = split(' ',$ARGV[0]);
-foreach $as (@as_list) {
-    if ($as =~ /[^\d\s]/ || $as < 1 || $as > 4294967294) { exit 1;}
+use strict;
+
+my @as_list = split( ' ', $ARGV[0] );
+foreach my $as (@as_list) {
+    exit 1 if ( $as =~ /[^\d\s]/ || $as < 1 || $as > 4294967294 );
 }
-if (scalar(@as_list) > 24) {
-    print "Error: max 24 as paths";
-    exit 1;
-}
+
+die "Error: max 24 as path\n" if ( scalar(@as_list) > 24 );
+
 exit 0;
