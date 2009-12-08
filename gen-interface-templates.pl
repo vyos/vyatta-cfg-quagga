17a77a2161625348b1f1c08b1f1df44471e77ac7
Stephen Hemminger <stephen.hemminger@vyatta.com>
2009-12-07 16:01:42 -0800

    Add VIF support for wireless

--- gen-interface-templates.pl
+++ gen-interface-templates.pl
@@ -35,6 +35,7 @@ my %interface_hash = (
     'ethernet/node.tag/vif/node.tag'                => '$VAR(../@).$VAR(@)',
     'ethernet/node.tag/vif/node.tag/pppoe/node.tag' => 'pppoe$VAR(@)',
     'wireless/node.tag'                             => '$VAR(@)',
+    'wireless/vif/node.tag'                         => '$VAR(../@).$VAR(@)',
     'pseudo-ethernet/node.tag'                      => '$VAR(@)',
 #   'pseudo-ethernet/node.tag/vif/node.tag'         => '$VAR(../@).$VAR(@)',
     'bonding/node.tag'                              => '$VAR(@)',
