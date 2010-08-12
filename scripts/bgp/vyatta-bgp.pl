a76640992a3b4c1aa54a0af3ef8d842423eb0fdc
Stephen Hemminger <stephen.hemminger@vyatta.com>
2010-08-12 08:45:50 -0400

    Add missing step for peer group ttl-security
    Bug 5809
    (cherry picked from commit 1f3b94d092ebdb9d682b66911738b6ce506f7b04)

--- scripts/bgp/vyatta-bgp.pl
+++ scripts/bgp/vyatta-bgp.pl
@@ -1036,6 +1036,10 @@ my %qcom = (
       set => 'router bgp #3 ; neighbor #5 timers connect #8',
       del => 'router bgp #3 ; no neighbor #5 timers connect #8',
   },
+  'protocols bgp var peer-group var ttl-security' => {
+      set => undef,
+      del => undef,
+  },
   'protocols bgp var peer-group var ttl-security hops' => {
       set => 'router bgp #3 ; neighbor #5 ttl-security hops #8',
       del => 'router bgp #3 ; no neighbor #5 ttl-security hops #8',
